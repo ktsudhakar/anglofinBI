a55b61f8dbe2668682cb1505f47d68ef518d5517
Sudhakar KT <sudhakar@tillidsoft.com>
2022-03-23 14:12:45 +0530

    Search result page markup and UI update.

--- js/main.js
+++ js/main.js
@@ -7,4 +7,18 @@ $('document').ready( function () {
   $('.filter-block, .close-panel').click (function () {
     $('.slide-out-panel-right').toggleClass('show');
   });
+  const file = document.querySelector('#file');
+  file.addEventListener('change', (e) => {
+    // Get the selected file
+    const [file] = e.target.files;
+    // Get the file name and size
+    const { name: fileName, size } = file;
+    // Convert size in bytes to kilo bytes
+    const fileSize = (size / 1000).toFixed(2);
+    // Set the text content
+    const fileNameAndSize = `${fileName} - ${fileSize}KB`;
+    const element = document.querySelector(".file-name");
+    element.classList.add("file-active");
+    element.textContent = fileNameAndSize;
+  });
 });
